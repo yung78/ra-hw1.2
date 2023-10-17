5ee026e183863d0c416587c9610e0c7c8ebd7f7d
yung78 <alexdrum@mail.ru>
2023-10-17 13:03:19 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/main.css';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+class ShopItemClass extends React.Component {
+  render() {
+    return (
+      <div class="main-content">
+        <h2>{ item.brand }</h2>
+        <h1>{ item.title }</h1>
+        <h3>{ item.description }</h3>
+        <div class="description">{ item.descriptionFull }</div>
+        <div class="highlight-window mobile">
+          <div class="highlight-overlay"></div>
+        </div>
+        <div class="divider"></div>
+        <div class="purchase-info">
+          <div class="price">{ item.currency+item.price+'.00' }</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="background-element">
     </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemClass item={item} />
+    </div>
+  </div>
   );
 }
 
